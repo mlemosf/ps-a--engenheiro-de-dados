d7e1ecd690bb69ad7d391fb67434a424fe03f04a
Matheus Fernandes <matheuslemosf@protonmail.com>
2026-02-22 19:22:31 -0300

    feat: Tabelas silver

--- src/prefect/main.py
+++ src/prefect/main.py
@@ -5,10 +5,11 @@ from prefect_sqlalchemy import SqlAlchemyConnector
 import requests
 import pandas as pd
 from sqlalchemy.types import JSON
+from datetime import datetime
 
 
 @task()
-def read_api(path):
+def read_api(path) -> pd.DataFrame:
     logger = get_run_logger()
     try:
         response = requests.get(f"https://fakestoreapi.com/{path}")
@@ -23,7 +24,22 @@ def read_api(path):
         logger.error(f"Error: {str(e)}")
 
 @task
-def store_users_in_database(df):
+def read_table_into_df(tablename):
+    with SqlAlchemyConnector.load("postgres-credentials") as connector:
+        engine = connector.get_engine()
+        df = pd.read_sql_table(
+            tablename,
+            con=engine
+        )
+        return df
+
+# Bronze tables
+@flow
+def bronze_users():
+    # Read users from API
+    df = read_api("/users")
+
+    # Store bronze_users
     logger = get_run_logger()
     with SqlAlchemyConnector.load("postgres-credentials") as connector:
         engine = connector.get_engine()
@@ -36,9 +52,10 @@ def store_users_in_database(df):
         )
         logger.info(f"Stored {len(df)} users to database")
 
+@flow
+def bronze_products():
+    df = read_api("/products")
 
-@task
-def store_products_in_database(df):
     logger = get_run_logger()
     with SqlAlchemyConnector.load("postgres-credentials") as connector:
         engine = connector.get_engine()
@@ -51,9 +68,10 @@ def store_products_in_database(df):
         )
         logger.info(f"Stored {len(df)} products to database")
 
+@flow
+def bronze_carts():
+    df = read_api("/carts")
 
-@task
-def store_carts_in_database(df):
     logger = get_run_logger()
     with SqlAlchemyConnector.load("postgres-credentials") as connector:
         engine = connector.get_engine()
@@ -65,34 +83,123 @@ def store_carts_in_database(df):
             dtype={'products': JSON}
         )
         logger.info(f"Stored {len(df)} carts to database")
-# Bronze tables
+
+# Silver tables
 @flow
-def bronze_users():
-    # Read users from API
-    df = read_api("/users")
+def silver_users():
+    logger = get_run_logger()
+    df = read_table_into_df("bronze_users")
+    name = df["name"].apply(pd.Series)
 
-    # Store bronze_users
-    store_users_in_database(df)
+    df2 = df[["id", "email", "username", "phone"]]
+    df2["first_name"] = name["firstname"]
+    df2["last_name"] = name["lastname"]
+    df2["ingestion_date"] = datetime.today()
+
+    logger.info(df2)
+
+    with SqlAlchemyConnector.load("postgres-credentials") as connector:
+        engine = connector.get_engine()
+        df2.to_sql(
+            name="silver_users",
+            con=engine,
+            if_exists="append",
+            index=False
+        )
+        logger.info(f"Stored {len(df)} users to database")
 
 @flow
-def bronze_products():
-    df = read_api("/products")
+def silver_geolocation():
+    logger = get_run_logger()
+    df = read_table_into_df("bronze_users")
+    address = pd.json_normalize(df["address"], sep="_")
+    logger.info(address)
 
-    store_products_in_database(df)
+    df2 = df[["id"]]
+    df2["city"] = address["city"]
+    df2["lat"] = address["geolocation_lat"]
+    df2["long"] = address["geolocation_long"]
+    df2["street"] = address["street"]
+    df2["number"] = address["number"]
+    df2["zip_code"] = address["zipcode"]
+    df2["ingestion_date"] = datetime.today()
 
+    df2.rename(columns={"id": "user_id"})
+
+    logger.info(df2)
+
+    with SqlAlchemyConnector.load("postgres-credentials") as connector:
+        engine = connector.get_engine()
+        df2.to_sql(
+            name="silver_geolocation",
+            con=engine,
+            if_exists="append",
+            index=False
+        )
+        logger.info(f"Stored {len(df)} users to database")
 
 @flow
-def bronze_carts():
-    df = read_api("/carts")
+def silver_products():
+    logger = get_run_logger()
+    df = read_table_into_df("bronze_products")
+    rating = pd.json_normalize(df["rating"], sep="_")
 
-    store_carts_in_database(df)
+    df2 = df[["id","title", "price", "description", "category", "image"]]
+    df2["rating_rate"] = rating["rate"]
+    df2["rating_count"] = rating["count"]
+    df2["ingestion_date"] = datetime.today()
+
+    df2.rename(columns={"id": "user_id"})
+
+    logger.info(df2)
+
+    with SqlAlchemyConnector.load("postgres-credentials") as connector:
+        engine = connector.get_engine()
+        df2.to_sql(
+            name="silver_products",
+            con=engine,
+            if_exists="append",
+            index=False
+        )
+        logger.info(f"Stored {len(df)} users to database")
+
+@flow
+def silver_carts():
+    logger = get_run_logger()
+    df = read_table_into_df("bronze_carts")
+
+    df2 = df.explode("products")
+    products = pd.json_normalize(df2["products"])
+    df2["product_id"] = products["productId"]
+    df2["product_quantity"] = products["quantity"]
+    df2["ingestion_date"] = datetime.today()
+    df2.drop(["products", "__v"], axis=1, inplace=True)
+    logger.info(df2)
+
+    df2.rename(columns={"userId": "user_id"}, inplace=True)
+
+    with SqlAlchemyConnector.load("postgres-credentials") as connector:
+        engine = connector.get_engine()
+        df2.to_sql(
+            name="silver_carts",
+            con=engine,
+            if_exists="append",
+            index=False
+        )
+        logger.info(f"Stored {len(df)} users to database")
 
 @flow
 def main():
-    # Brone tables
+    # Bronze tables
     bronze_users()
     bronze_products()
     bronze_carts()
+
+    # Silver tables
+    silver_users()
+    silver_geolocation()
+    silver_products()
+    silver_carts()
 
 
 #if __name__ == "__main__":
